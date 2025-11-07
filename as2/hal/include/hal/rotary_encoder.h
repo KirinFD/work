6ae87fb11b70aaff1807060ddbcc9abd053da2ef
KirinFD <colby.ho@gmail.com>
2025-11-07 13:48:49 -0800

    i hate everything

--- as2/hal/include/hal/rotary_encoder.h
+++ as2/hal/include/hal/rotary_encoder.h
@@ -12,20 +12,19 @@ typedef void (*re_button_cb)(bool pressed, int64_t timestamp_ns, void *user);
 typedef void (*re_position_cb)(int32_t position, int32_t delta, int64_t timestamp_ns, void *user);
 
 typedef struct re_config {
-    const char *chip;          // "/dev/gpiochip0" or "gpiochip0" (will be prefixed internally)
+    const char *chip;     
     unsigned line_a;
     unsigned line_b;
 
     bool a_active_low;
     bool b_active_low;
 
-    unsigned steps_per_detent; // transitions per detent (typ 4)
+    unsigned steps_per_detent;
 
-    // Bias hints: -1 means "do not set (leave default)"
-    int a_bias;                // GPIOD_LINE_BIAS_* or -1
+    int a_bias;           
     int b_bias;
 
-    const char *consumer;      // label shown to kernel
+    const char *consumer;    
 } re_config;
 
 int  re_create(const re_config *cfg, re_encoder **out);
